5c4ef0e1bc37fa4148e300372e14058a13d1ee10
Ashok Kumar S <ashokslsk@gmail.com>
2019-04-11 14:57:51 +0530

    Initial logics and UI

---
+++ app/src/main/java/com/androidabcd/ashokslsk/mylivnassignment/AvatarLoader/AvatarCallBack.kt
@@ -0,0 +1,13 @@
+package com.androidabcd.ashokslsk.mylivnassignment.AvatarLoader
+
+import android.graphics.Bitmap
+
+/**
+ * Created by Srinivasa Ashok Kumar on 11/04/19.
+ */
+interface AvatarCallBack {
+    fun onProgress(progress: Int) {}
+    fun onAvatarAvailable(bitmap: Bitmap) {}
+    fun onError(e: Exception) {}
+    fun onCancelled() {}
+}
